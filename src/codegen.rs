66cf3b70da6daad9ab6b13b64eede87927aad23b
Waqar Ahmed <waqar.17a@gmail.com>
2024-02-27 21:59:04 +0500

    generate expression

--- src/codegen.rs
+++ src/codegen.rs
@@ -3,6 +3,7 @@ use std::io::Write;
 use crate::{
     node::Node,
     parser::{Object, VarObject},
+    ty::Type,
 };
 
 const ARG_REGS8: [&'static str; 6] = ["%dil", "%sil", "%dl", "%cl", "%r8b", "%r9b"];
@@ -15,6 +16,7 @@ struct CodeGenerator<'a> {
     program: &'a Vec<Object>,
     counter: usize,
     current_fn_name: String,
+    depth: usize,
 }
 
 impl CodeGenerator<'_> {
@@ -44,13 +46,179 @@ impl CodeGenerator<'_> {
         let _ = writeln!(self.writer, "{s}");
     }
 
+    fn push(&mut self) {
+        self.emit("  push %rax");
+        self.depth += 1;
+    }
+
+    fn pop(&mut self, reg: &str) {
+        self.emit(&format!("  pop {}", reg));
+        self.depth -= 1;
+    }
+
     fn next_count(&mut self) -> usize {
         let c = self.counter;
         self.counter += 1;
         c
     }
 
-    fn gen_expr(&mut self, node: &Node) {}
+    fn load(&mut self, ty: &Type) {
+        if matches!(
+            ty,
+            Type::Struct { .. } | Type::Union { .. } | Type::Array { .. }
+        ) {
+            // If it is an array, do not attempt to load a value to the
+            // register because in general we can't load an entire array to a
+            // register. As a result, the result of an evaluation of an array
+            // becomes not the array itself but the address of the array.
+            // This is where "array is automatically converted to a pointer to
+            // the first element of the array in C" occurs.
+            return;
+        }
+
+        match ty.size() {
+            1 => self.emit("  mov %al, (%rdi)"),
+            2 => self.emit("  mov %ax, (%rdi)"),
+            4 => self.emit("  mov %eax, (%rdi)"),
+            8 => self.emit("  mov %rax, (%rdi)"),
+            _ => {
+                eprintln!("Unexpected ty size {}", ty.size());
+                panic!();
+            }
+        }
+    }
+
+    fn store(&mut self, ty: &Type) {
+        self.pop("%rdi");
+
+        if matches!(ty, Type::Struct { .. } | Type::Union { .. }) {
+            for i in 0..ty.size() {
+                self.emit(&format!("  mov {}(%rax), %r8b", i));
+                self.emit(&format!("  mov %r8b, {}(%rdi)", i));
+            }
+            return;
+        }
+
+        match ty.size() {
+            1 => self.emit("  mov %al, (%rdi)"),
+            2 => self.emit("  mov %ax, (%rdi)"),
+            4 => self.emit("  mov %eax, (%rdi)"),
+            8 => self.emit("  mov %rax, (%rdi)"),
+            _ => {
+                eprintln!("Unexpected ty size {}", ty.size());
+                panic!();
+            }
+        }
+    }
+
+    fn gen_address(&mut self, node: &Node) {
+        match node {
+            Node::Variable(v) => {
+                let var = v.var.as_var_object();
+                if var.is_local {
+                    self.emit(&format!("  lea {}(%rbp), %rax", var.offset.get()));
+                } else {
+                    self.emit(&format!("  lea {}(%rip), %rax", var.name));
+                }
+            }
+            Node::Dereference(d) => self.gen_expr(&*d.lhs),
+            Node::Comma(c) => {
+                self.gen_expr(&*c.lhs);
+                self.gen_address(&*c.rhs);
+            }
+            Node::StructMember(s) => {
+                self.gen_address(&*s.lhs);
+                self.emit(&format!("  add ${}, %rax", s.member.offset));
+            }
+            _ => {
+                eprintln!("Not an lvalue");
+                panic!();
+            }
+        }
+    }
+
+    fn gen_expr(&mut self, node: &Node) {
+        match node {
+            Node::Numeric(num) => self.emit(&format!("  mov ${}, %rax", num.val)),
+            Node::Neg(n) => {
+                self.gen_expr(&*n.lhs);
+                self.emit("  neg %rax");
+            }
+            Node::Variable(_) | Node::StructMember(_) => {
+                self.gen_address(node);
+                self.load(node.ty());
+            }
+            Node::Assign(a) => {
+                self.gen_address(&*a.lhs);
+                self.push();
+                self.gen_expr(&*a.rhs);
+                self.store(&a.ty);
+            }
+            Node::AddressOf(a) => self.gen_address(&*a.lhs),
+            Node::Dereference(d) => {
+                self.gen_expr(&*d.lhs);
+                self.load(&d.ty);
+            }
+            Node::StmtExpr(s) => {
+                for node in s.block_body.iter() {
+                    self.gen_stmt(node);
+                }
+            }
+            // TODO function call
+            Node::Comma(c) => {
+                self.gen_expr(&*c.lhs);
+                self.gen_expr(&*c.rhs);
+            }
+            Node::Cast(c) => {
+                self.gen_expr(&*c.lhs);
+                // TODO cast
+            }
+            _ => (),
+        }
+
+        let mut di = "";
+        let mut ax = "";
+        if let Some(b) = node.binary_node() {
+            self.gen_expr(&*b.lhs);
+            self.push();
+            self.gen_expr(&*b.rhs);
+            self.pop("%rdi");
+
+            if b.lhs.ty().size() == 8 {
+                ax = "%rax";
+                di = "%rdi";
+            } else {
+                ax = "%eax";
+                di = "%edi";
+            }
+        }
+
+        match node {
+            Node::Add(_) => self.emit(&format!("  add {}, {}", di, ax)),
+            Node::Sub(_) => self.emit(&format!("  sub {}, {}", di, ax)),
+            Node::Mul(_) => self.emit(&format!("  imul {}, {}", di, ax)),
+            Node::Div(div) => {
+                if div.ty.size() == 8 {
+                    self.emit("  cqo");
+                } else {
+                    self.emit("  cdq");
+                }
+                self.emit(&format!("  idiv {}", di));
+            }
+            Node::LessThan(_) | Node::LessThanEq(_) | Node::Eq(_) | Node::NotEq(_) => {
+                self.emit(&format!("  cmp {}, {}", di, ax));
+                match node {
+                    Node::Eq(_) => self.emit("  sete %al"),
+                    Node::NotEq(_) => self.emit("  setne %al"),
+                    Node::LessThanEq(_) => self.emit("  setle %al"),
+                    Node::LessThan(_) => self.emit("  setl %al"),
+                    _ => unreachable!(),
+                }
+                self.emit("  movzb %al, %rax");
+            }
+            _ => (),
+        }
+    }
 
     fn gen_stmt(&mut self, node: &Node) {
         match node {
@@ -217,6 +385,7 @@ pub fn generate(writer: &mut dyn Write, program: &Vec<Object>) {
         program,
         counter: 1,
         current_fn_name: String::new(),
+        depth: 0,
     };
     gen.generate();
 }

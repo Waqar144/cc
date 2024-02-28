7f3f1c225571cbf03f2f02f5023083400211487c
Waqar Ahmed <waqar.17a@gmail.com>
2024-02-28 14:08:38 +0500

    Write to file or stdout
    Very basic arg parsing

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,39 @@ fn main() {
     let file_name = args.iter().nth(1).unwrap();
     let path = std::path::Path::new(&file_name);
 
+    let mut codegen_out: Box<dyn std::io::Write> = if let Some(out) = args.iter().nth(2) {
+        if out != "-o" {
+            eprintln!("Unknown param {out}");
+            std::process::exit(1);
+        }
+        let Some(out_file) = args.iter().nth(3) else {
+            eprintln!("No output file name specified with -o");
+            std::process::exit(1);
+        };
+        let file = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&out_file);
+        if let Err(e) = file {
+            eprintln!("Failed to open file, error: {e}");
+            std::process::exit(1);
+        }
+        Box::new(file.unwrap())
+    } else {
+        Box::new(std::io::stdout())
+    };
+
     if !path.exists() {
         eprintln!("no such file {}", path.display());
         std::process::exit(1);
     }
 
     let contents = std::fs::read_to_string(path).expect("Failed to read file");
-    println!("LEN: {}", contents.len());
     let toks = lexer::tokenize(&contents);
 
     let mut parser = parser::Parser::new(&contents, &toks);
     parser.parse();
 
-    codegen::generate(&mut std::io::stdout(), &parser.globals);
+    codegen::generate(codegen_out.as_mut(), &parser.globals);
 }

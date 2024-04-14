7b7b4816d542024a9f0323584191cb1248535e99
Waqar Ahmed <waqar.17a@gmail.com>
2024-04-14 12:44:26 +0500

    Improve run_tests script a bit

--- run_tests.sh
+++ run_tests.sh
@@ -8,11 +8,13 @@ TESTFILES="cast sizeof typedef union struct arith control function variable stri
 
 for filename in $TESTFILES
 do
+    echo "TEST $filename"
 #     filename=$(basename -s .c $cfile)
     # preprocess using gcc
     gcc -o- -E -P -C $TESTDIR/$filename.c > testout/$filename.c
     # run our compiler
-    cargo run -- testout/$filename.c -o testout/$filename.s
+    ./target/debug/cc testout/$filename.c -o testout/$filename.s
+    #cargo run -- testout/$filename.c -o testout/$filename.s
     # compile output asm with gcc
     gcc -o testout/$filename testout/$filename.s -xc $TESTDIR/test.c
     # run the output
